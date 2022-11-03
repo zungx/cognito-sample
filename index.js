f283256e594fb8acfde7db5e31025c2a6c145217
zungx <vanzungx@gmail.com>
2022-11-04 00:55:59 +0700

    update env

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
 const bodyParser = require('body-parser');
 const AWS = require('aws-sdk');
 const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
